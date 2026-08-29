309fb620d2ad4bc7c054e853a06bfdbcb15a3684
soenneker <jake@soenneker.com>
2026-08-29 14:51:20 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Zendesk.Runners.OpenApiClient/Utils/Abstract/IFileOperationsUtil.cs
+++ src/Soenneker.Zendesk.Runners.OpenApiClient/Utils/Abstract/IFileOperationsUtil.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 
 namespace Soenneker.Zendesk.Runners.OpenApiClient.Utils.Abstract;
 
+/// <summary>
+/// Provides file cleanup and filesystem operations used by the generated-client update workflow.
+/// </summary>
 public interface IFileOperationsUtil
 {
     /// <summary>
--- src/Soenneker.Zendesk.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
+++ src/Soenneker.Zendesk.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
@@ -22,7 +22,7 @@ using System.Collections.Generic;
 
 namespace Soenneker.Zendesk.Runners.OpenApiClient.Utils;
 
-///<inheritdoc cref="IFileOperationsUtil"/>
+/// <inheritdoc cref="IFileOperationsUtil"/>
 public sealed class FileOperationsUtil : IFileOperationsUtil
 {
     private readonly ILogger<FileOperationsUtil> _logger;
